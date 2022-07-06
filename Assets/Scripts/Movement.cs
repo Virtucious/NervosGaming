5238ce35ed515bf94554c00a0d6030ab9e5af5c5
Virtucious <aman.rahman.0309@gmail.com>
2022-07-06 11:50:04 +0530

    Added the animation
    Need fix at movement script. Player doesnt go back to idle animation at the end of movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,24 +5,43 @@ public class Movement : MonoBehaviour
 {
     public LayerMask whatCanBeClickedOn;
     private NavMeshAgent myAgent;
+    public Animator playerAnimator;
 
     private void Start()
     {
         myAgent = GetComponent<NavMeshAgent>();
+        myAgent.speed = 0f;
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            myAgent.speed = 5f;
             Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+
             if (Physics.Raycast(myRay, out hit, 100, whatCanBeClickedOn))
             {
                 myAgent.SetDestination(hit.point);
             }
 
+            if (myAgent.transform.position == hit.point)
+            {
+                myAgent.speed = 0f;
+            }
 
+            if (myAgent.speed > 0f)
+            {
+                playerAnimator.SetBool("isWalking", true);
+            }
+            else
+            {
+                playerAnimator.SetBool("isWalking", false);
+            }
         }
+
+
+
     }
 }
