d77104932885aac9e8b1df52dd85a04213010167
Virtucious <aman.rahman.0309@gmail.com>
2022-07-06 16:58:42 +0530

    Completed initial menuAnimation

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,11 +7,17 @@ public class UIManager : MonoBehaviour
 {
 
     public Animator startButton;
+    public Animator menuButton;
     
     public void StartGame()
     {
         SceneManager.LoadScene("2");
         startButton.SetBool("isHidden", true);
+    }
+
+    public void onActive()
+    {
+        menuButton.SetBool("onClick", true);
     }
     
     void Start()
